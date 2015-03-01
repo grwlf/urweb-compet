bf5c0fb6c82bbfd050436f5300af30f2e0047fc4
Sergey Mironov <grrwlf@gmail.com>
2015-03-01 09:43:22 +0000

    Use uwapp_postgres

--- Cake_Compet.hs
+++ Cake_Compet.hs
@@ -8,9 +8,9 @@ import qualified Cake_Bootstrap as Bootstrap
 import qualified Cake_MonadPack as MonadPack
 import Cake_Compet_P
 
-app = do
+(app,db) = do
   let pn = file "Compet.urp"
-  uwapp "-dbms postgres" pn $ do
+  uwapp_postgres pn $ do
     allow mime "text/javascript"
     allow mime "text/css"
     allow mime "image/jpeg"
@@ -21,10 +21,8 @@ app = do
     allow url "http://github.com*"
     allow url "http://impredicative.com*"
     allow url "http://hit.msk.ru*"
-    database ("dbname="++(takeBaseName pn))
     safeGet "Compet/main"
     safeGet "Compet/init"
-    sql (pn.="sql")
     library Prelude.lib
     library Bootstrap.lib
     library MonadPack.lib
@@ -40,14 +38,6 @@ app = do
 main = writeDefaultMakefiles $ do
 
   a <- app
-
-  db <- rule $ do
-    let sql = urpSql (toUrp a)
-    let dbn = takeBaseName sql
-    shell [cmd|dropdb --if-exists $(string dbn)|]
-    shell [cmd|createdb $(string dbn)|]
-    shell [cmd|psql -f $(sql) $(string dbn)|]
-    shell [cmd|touch @(sql.="db")|]
 
   rule $ do
     phony "dropdb"
